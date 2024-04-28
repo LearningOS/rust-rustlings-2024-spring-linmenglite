8c0a187f7c59f6f639b6dbce709edfa00b0cb129
linmenglite <140476514+linmenglite@users.noreply.github.com>
2024-04-28 13:44:09 +0000

    update

--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -12,7 +12,7 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::borrow::Cow;
 
@@ -48,7 +48,9 @@ mod tests {
         let slice = [0, 1, 2];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
-            // TODO
+            _ => Ok(()),
+            Cow::Owned(_)=> Err("Expected owned value"),
+        }
         }
     }
 
@@ -60,9 +62,11 @@ mod tests {
         let slice = vec![0, 1, 2];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
-    }
+        }
+   
 
     #[test]
     fn owned_mutation() -> Result<(), &'static str> {
@@ -72,7 +76,9 @@ mod tests {
         let slice = vec![-1, 0, 1];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
-    }
-}
+        }
+    
+
--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,7 +8,7 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -26,7 +26,9 @@ fn main() {
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        // TODO: a struct is returned from thread::spawn, can you use it?  
+            let result = handle.join().unwrap(); // 等待线程完成并获取结果  
+            results.push(result); 
     }
 
     if results.len() != 10 {
@@ -38,3 +40,4 @@ fn main() {
         println!("thread {} took {}ms", i, result);
     }
 }
+
